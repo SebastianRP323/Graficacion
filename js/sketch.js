68a175f6e8cfb7ec102cf44778b645b29c6746a9
SebastianRP323 <l24170694@culiacan.tecnm.mx>
2026-02-17 10:20:05 -0700

    Add files via upload

---
+++ js/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+
+  createCanvas(400, 400);
+  background(100);
+}
+function draw() {
+  // line(80,20,125,150);
+  stroke(0);
+  line(0,250,800,250);
+  fill(0,0,255);
+  noStroke();
+  circle(400,250,20);
+
+}
